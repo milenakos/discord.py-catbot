ab4b3aa931da195430775911376fdcf3a8e0f99f
Lia Milenakos <lia@minkos.lol>
2025-09-27 18:10:32 +0000

    remove logging

--- discord/http.py
+++ discord/http.py
@@ -595,24 +595,24 @@ class HTTPClient:
         route_key = route.key
 
         # cat bot start
-        target_abs = os.path.abspath("/root/main.py")
-        frame = sys._getframe(1)
-        last_match = None
-        depth = 0
-        while frame is not None and depth < 100:
-            co = frame.f_code
-            frame_path = os.path.abspath(co.co_filename)
-            if frame_path == target_abs:
-                last_match = (frame_path, frame.f_lineno, co.co_name)
-            else:
-                if last_match is not None:
-                    break
-            frame = frame.f_back
-            depth += 1
-        if last_match:
-            path, lineno, func = last_match
-            print(f"[cbl] {lineno}")
-            print(f"[cbp] {route.major_parameters}")
+        # target_abs = os.path.abspath("/root/main.py")
+        # frame = sys._getframe(1)
+        # last_match = None
+        # depth = 0
+        # while frame is not None and depth < 100:
+        #     co = frame.f_code
+        #     frame_path = os.path.abspath(co.co_filename)
+        #     if frame_path == target_abs:
+        #         last_match = (frame_path, frame.f_lineno, co.co_name)
+        #     else:
+        #         if last_match is not None:
+        #             break
+        #     frame = frame.f_back
+        #     depth += 1
+        # if last_match:
+        #     path, lineno, func = last_match
+        #     print(f"[cbl] {lineno}")
+        #     print(f"[cbp] {route.major_parameters}")
         # cat bot end
 
         bucket_hash = None
